5444ce36c92c536025a1a4722a86143282472397
Edwin030790 <92564973+Edwin030790@users.noreply.github.com>
2022-07-24 16:56:40 -0500

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,50 @@
+$servername = AZR-EdwinTestVM
+
+
+[CmdletBinding()]
+
+param 
+( 
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$DomainName,
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$AdmincredsUserName,
+    [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$_Admincreds_Password
+)
+
+$username = $AdmincredsUserName
+$password = ConvertTo-SecureString -AsPlainText $_Admincreds_Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential ($username, $password)
+
+
+
+
+#Install and Scripts folder
+New-Item -Path 'C:\Install' -ItemType Directory -verbose
+New-Item -Path 'C:\Scripts' -ItemType Directory -verbose
+
+#Set Time Zone
+Set-TimeZone -Id "Central Standard Time" -verbose
+
+#Rename Server
+Rename-Computer -NewName "$servername" -verbose
+
+
+#Allows RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0 -verbose
+
+#Unchecks NLA Requirement for RDP
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name "UserAuthentication" -Value 0 -verbose
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name "SecurityLayer" -Value 0 -verbose
+
+#Disables UAC
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0 -verbose
+
+#Sets CD-ROM drive to  Z
+
+Get-CimInstance -ClassName Win32_Volume -Filter "DriveLetter = 'E:'" | Set-CimInstance -Property @{DriveLetter ='Z:'}
+
+#Adds Server to Domain
+Add-Computer -DomainName infinite.local -Credential $Cred -Restart -Force
+
+
+
+#Add-Computer -ComputerName Server01 -LocalCredential Server01\Admin01 -DomainName Domain02 -Credential $Cred -Restart -Force
